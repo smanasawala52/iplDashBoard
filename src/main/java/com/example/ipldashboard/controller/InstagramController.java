cd6b59a2c916c53c2ebb9cef78e0217911fa7413
smanasawala52 <67199826+smanasawala52@users.noreply.github.com>
2022-07-20 16:51:23 +0530

    Update InstagramController.java

--- src/main/java/com/example/ipldashboard/controller/InstagramController.java
+++ src/main/java/com/example/ipldashboard/controller/InstagramController.java
@@ -7,8 +7,12 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.zip.GZIPInputStream;
+
+import javax.websocket.server.PathParam;
 
 import org.codehaus.jettison.json.JSONArray;
 import org.codehaus.jettison.json.JSONObject;
@@ -29,8 +33,7 @@ public class InstagramController {
 	@GetMapping("/auth")
 	public ModelAndView auth(
 			@RequestParam final Map<String, String> queryParams) {
-		ModelAndView modelAndView = new ModelAndView("instadetails");
-		modelAndView.addObject("slug", dataService.getSlug());
+		String accessToken = "";
 		if (queryParams.get("code") != null) {
 			String code = queryParams.get("code");
 			code = code.replace("_#", "");
@@ -66,22 +69,15 @@ public class InstagramController {
 				System.out.println(http.getContent());
 				http.disconnect();
 				JSONObject json = new JSONObject(line);
-				String accessToken = (String) json.get("access_token");
+				accessToken = (String) json.get("access_token");
 				System.out.println("access_token: " + accessToken);
-
-				modelAndView
-						.addAllObjects(populateInstaDataAccessToken(accessToken)
-								.getModelMap());
-
-				// JSONObject jsonInstagram = new JSONObject(line);
-
 			} catch (Exception e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 
 		}
-		return modelAndView;
+		return new ModelAndView("redirect:" + "/instadetails/" + accessToken);
 	}
 
 	@GetMapping("/instadetails/{accessToken}")
@@ -159,28 +155,147 @@ public class InstagramController {
 				}
 				modelAndView.addObject("mediaList", mediaList);
 			}
-			obj = new URL(
-					"https://www.instagram.com/" + json.get("username") + "/");
-			con = (HttpURLConnection) obj.openConnection();
+			populateInstaUserDetails(modelAndView,
+					(String) json.get("username"));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return modelAndView;
+	}
+
+	private void populateInstaUserDetails(ModelAndView modelAndView,
+			String username) {
+		try {
+			// basic info
+			URL obj = new URL(
+					"https://i.instagram.com/api/v1/users/web_profile_info/?username="
+							+ username);
+			HttpURLConnection con = (HttpURLConnection) obj.openConnection();
 			// optional default is GET
+			con.setDoOutput(true);
+			con.setRequestProperty("Content-Type", "application/json");
 			con.setRequestMethod("GET");
 			// add request header
-			con.setRequestProperty("User-Agent", "Mozilla/5.0");
-			responseCode = con.getResponseCode();
+			con.setRequestProperty("Accept-Encoding", "gzip");
+			con.setRequestProperty("cookie",
+					"sessionid=8485620030:WM9DLpiiVsYD4E:11:AYf2u72g7nTjxWXW-v4X-Z46fvCADtb4yQd-ZxZJFQ;");
+			con.setRequestProperty("user-agent",
+					"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36");
+			con.setRequestProperty("x-ig-app-id", "936619743392459");
+
+			int responseCode = con.getResponseCode();
+			// System.out.println(con.getHeaderFields());
 			System.out.println("\nSending 'GET' request to URL : " + obj);
 			System.out.println("Response Code : " + responseCode);
-			in = new BufferedReader(
-					new InputStreamReader(con.getInputStream()));
-			inputLine = "";
-			response = new StringBuffer();
+			// reader = new InputStreamReader(con.getInputStream());
+			BufferedReader in = new BufferedReader(new InputStreamReader(
+					new GZIPInputStream(con.getInputStream())));
+			String inputLine = "";
+			StringBuffer response = new StringBuffer();
 			while ((inputLine = in.readLine()) != null) {
 				response.append(inputLine);
 			}
 			in.close();
-			line = response.toString();
+			String line = response.toString();
 			// print in String
-			// System.out.println(line);
+			System.out.println(line);
+			JSONObject jsonUserInfo = new JSONObject(line);
+			String owner = jsonUserInfo.getJSONObject("data")
+					.getJSONObject("user").getString("id");
+			modelAndView.addObject(owner, owner);
+			modelAndView.addAllObjects(
+					populateInstaUserFollowerPage(owner, "").getModelMap());
+			modelAndView.addObject("followers_count",
+					jsonUserInfo.getJSONObject("data").getJSONObject("user")
+							.getJSONObject("edge_followed_by")
+							.getString("count"));
+
+			modelAndView.addObject("biography",
+					jsonUserInfo.getJSONObject("data").getJSONObject("user")
+							.getString("biography"));
+			modelAndView.addObject("profilePicUrl",
+					jsonUserInfo.getJSONObject("data").getJSONObject("user")
+							.getString("profile_pic_url"));
 		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	@GetMapping("/instaFollowers/{owner}/{maxId}")
+	public ModelAndView populateInstaUserFollowerPage(
+			@PathParam("owner") String owner,
+			@PathParam("maxId") String maxId) {
+		ModelAndView modelAndView = new ModelAndView("instadetails");
+		modelAndView.addObject("slug", dataService.getSlug());
+		try {
+			// basic info
+			URL obj = new URL("https://i.instagram.com/api/v1/friendships/"
+					+ owner
+					+ "/followers/?count=12&search_surface=follow_list_page&max_id="
+					+ maxId);
+			HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+			// optional default is GET
+			con.setDoOutput(true);
+			con.setRequestProperty("Content-Type", "application/json");
+			con.setRequestMethod("GET");
+			// add request header
+			con.setRequestProperty("Accept-Encoding", "gzip");
+			con.setRequestProperty("cookie",
+					"sessionid=8485620030:WM9DLpiiVsYD4E:11:AYf2u72g7nTjxWXW-v4X-Z46fvCADtb4yQd-ZxZJFQ;");
+			con.setRequestProperty("user-agent",
+					"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36");
+			con.setRequestProperty("x-ig-app-id", "936619743392459");
+
+			int responseCode = con.getResponseCode();
+			// System.out.println(con.getHeaderFields());
+			System.out.println("\nSending 'GET' request to URL : " + obj);
+			System.out.println("Response Code : " + responseCode);
+			// reader = new InputStreamReader(con.getInputStream());
+			BufferedReader in = new BufferedReader(new InputStreamReader(
+					new GZIPInputStream(con.getInputStream())));
+			String inputLine = "";
+			StringBuffer response = new StringBuffer();
+			while ((inputLine = in.readLine()) != null) {
+				response.append(inputLine);
+			}
+			in.close();
+			String line = response.toString();
+			// print in String
+			System.out.println(line);
+			JSONObject jsonUserInfo = new JSONObject(line);
+			Map<String, List<String>> followers = new LinkedHashMap<>();
+			JSONArray followerJson = jsonUserInfo.getJSONArray("users");
+			modelAndView.addObject(owner, owner);
+			for (int i = 0; i < followerJson.length(); i++) {
+				JSONObject object = (JSONObject) followerJson.get(i);
+				try {
+					String pkFollower = object.getString("pk");
+					String usernameFollower = object.getString("username");
+					String fullNameFollower = object.getString("full_name");
+					String profilePicUrlFollower = object
+							.getString("profile_pic_url");
+					List<String> follower = followers.get(pkFollower);
+					if (follower == null) {
+						follower = new ArrayList<String>();
+					}
+					follower.add(pkFollower);
+					follower.add(usernameFollower);
+					follower.add(fullNameFollower);
+					if (profilePicUrlFollower != null) {
+						follower.add(profilePicUrlFollower);
+					}
+					followers.put(pkFollower, follower);
+				} catch (Exception ex) {
+					ex.printStackTrace();
+				}
+			}
+			modelAndView.addObject("followers", followers);
+			modelAndView.addObject("maxId",
+					jsonUserInfo.getString("next_max_id"));
+			System.out.println(followers);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		return modelAndView;
